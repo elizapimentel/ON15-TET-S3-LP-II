315f0d0ed83b327f8e371687a056f967c77c678e
elizapimentel <elizapimentel@hotmail.com>
2022-04-14 18:06:58 -0300

    Subindo desafio aula 03

--- desafio.js
+++ desafio.js
@@ -10,54 +10,82 @@ let clienteLilit = [
     {produto: 'Salto agulha 39', valor: 250.0}, 
     {produto: 'Tênis casual preto', valor: 120.0}, 
     {produto: 'meia calça transparente', valor: 30.0}]
+    
+let cliente2 = [
+    {produto: 'Cinto preto', valor: 22.0}, 
+    {produto: 'Bolsa pequena', valor: 49.0}, 
+    {produto: 'Calça preta', valor: 100.0}, 
+    {produto: 'Jaqueta Jeans', valor: 300.0}, 
+    {produto: 'Blusa simples', valor: 35.0} ]
+    
+let cliente3 = [
+    {produto: 'Blusa preta gola alta', valor: 60.0}, 
+    {produto: 'Short verde canelado', valor: 80.0}, 
+    {produto: 'Salto agulha 39', valor: 250.0}, 
+    {produto: 'Tênis casual preto', valor: 120.0}, 
+    {produto: 'meia calça transparente', valor: 30.0}]    
 
-function realizarCompra(comprasDaCliente) {
-    let valorTotal = 0 // variável com a soma de todos os valores dos produtos sem desconto
-    let descontoAplicado = 0 // variável com a soma de todos os descontos aplicados
-    let valorFinal = 0 // variável com a soma de todos os valores dos produtos com desconto
-    let quantidadeDeProdutos = comprasDaCliente.length
+// var para definir qual cliente deseja calcular dentro de todas funções    
+let cliente = cliente2
 
-    for(let item of comprasDaCliente) {
-        valorTotal += item.valor
-        if (item.valor >= 200.0) {
-            descontoAplicado += (item.valor * 0.5)
-            valorFinal += (item.valor * 0.5)
-        } else if (item.valor >= 100.0) {
-            descontoAplicado += (item.valor * 0.2)
-            valorFinal += (item.valor * 0.8)
-        } else if (item.valor >= 80.0) {
-            descontoAplicado += (item.valor * 0.1)
-            valorFinal += (item.valor * 0.9)
-        } else if (item.valor >= 50.0) {
-            descontoAplicado += (item.valor * 0.05)
-            valorFinal += (item.valor * 0.95)
-        } else {
-            valorFinal += item.valor
+// valorTotal
+function calcularTotal(cliente){
+    let valorTotal=0
+    valorTotal = cliente.reduce((acesso, produto) => acesso + produto.valor, 0)
+        return valorTotal
+    
+    } 
+
+// valorDesconto
+
+function calcularDesconto(cliente){
+    let valorDesconto = 0
+    
+    for(let item of cliente){
+        if(item.valor >= 200){
+            valorDesconto += (item.valor*0.5) 
+        }else if(item.valor >= 100){
+            valorDesconto += (item.valor*0.2)
+        }else if(item.valor >=80){
+            valorDesconto += (item.valor*0.1)
+        }else if(item.valor >= 50){
+            valorDesconto += (item.valor*0.05)
+        }else{
+            "Essa peça não possui desconto"            
         }
     }
+        return valorDesconto
+}
+ 
+//valor Final com descontos
 
-    let dataDaCompra = new Date()
-    console.log(dataDaCompra)
+let valorFinal = calcularTotal(cliente) - calcularDesconto(cliente)
+let dataCompra = new Date()
 
-    let notaFiscal = {
-        "Valor Total": `R$${valorTotal.toFixed(2).replace('.', ',')}`,
-        "Desconto Aplicado": `R$${descontoAplicado.toFixed(2).replace('.', ',')}`,
-        "Valor Final": `R$${valorFinal.toFixed(2).replace('.', ',')}`,
-        "Data de Compra": dataDaCompra.toLocaleDateString('pt-BR', {
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric'
-        })
-    }
+// let dataCompra = new Date() 
 
-    if(quantidadeDeProdutos > 10 || valorFinal > 800.0) {
-        return console.table({
+let notaFiscal = {
+    "Valor Total" : `R$${calcularTotal(cliente).toFixed(2).replace('.', ',')}`,
+    "Valor Desconto" : `R$${calcularDesconto(cliente).toFixed(2).replace('.', ',')}`,
+    "Valor Final" : `R$${valorFinal.toFixed(2).replace('.', ',')}`,
+    "Data de Compra": dataCompra.toLocaleDateString('pt-BR', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    })
+    
+}
+
+// Ultimo cupom
+function receberCupom(cliente){
+    if (cliente.length > 10 || valorFinal > 800.0){
+        return console.table ({
             ...notaFiscal,
-            "Bônus": 'Você ganhou um cupom de R$50,00 para a sua prõxima compra'
-        })
-    } else {
+            "Bonus" : "Você ganhou um cupom de R$50,00 para sua próxima compra"})
+
+    }else{
         return console.table(notaFiscal)
     }
 }
 
-realizarCompra(clienteLilit)
+receberCupom(cliente)
--- revisao.js
+++ revisao.js
@@ -117,3 +117,68 @@ if(idh >= 0 && idh < 0.5) {
     console.log('IDH inválido')
 }
 
+//Desafio
+
+let clienteLilit = [
+    {produto: 'Bolsa pequena', valor: 49.0}, 
+    {produto: 'Cinto preto', valor: 22.0}, 
+    {produto: 'Jaqueta Jeans', valor: 300.0}, 
+    {produto: 'Calça preta', valor: 100.0}, 
+    {produto: 'Blusa simples', valor: 35.0}, 
+    {produto: 'Calça jeans clara', valor: 130.0}, 
+    {produto: 'Blusa preta gola alta', valor: 60.0}, 
+    {produto: 'Short verde canelado', valor: 80.0}, 
+    {produto: 'Salto agulha 39', valor: 250.0}, 
+    {produto: 'Tênis casual preto', valor: 120.0}, 
+    {produto: 'meia calça transparente', valor: 30.0}]
+
+function realizarCompra(comprasDaCliente) {
+    let valorTotal = 0 // variável com a soma de todos os valores dos produtos sem desconto
+    let descontoAplicado = 0 // variável com a soma de todos os descontos aplicados
+    let valorFinal = 0 // variável com a soma de todos os valores dos produtos com desconto
+    let quantidadeDeProdutos = comprasDaCliente.length
+
+    for(let item of comprasDaCliente) {
+        valorTotal += item.valor
+        if (item.valor >= 200.0) {
+            descontoAplicado += (item.valor * 0.5)
+            valorFinal += (item.valor * 0.5)
+        } else if (item.valor >= 100.0) {
+            descontoAplicado += (item.valor * 0.2)
+            valorFinal += (item.valor * 0.8)
+        } else if (item.valor >= 80.0) {
+            descontoAplicado += (item.valor * 0.1)
+            valorFinal += (item.valor * 0.9)
+        } else if (item.valor >= 50.0) {
+            descontoAplicado += (item.valor * 0.05)
+            valorFinal += (item.valor * 0.95)
+        } else {
+            valorFinal += item.valor
+        }
+    }
+
+    let dataDaCompra = new Date()
+    console.log(dataDaCompra)
+
+    let notaFiscal = {
+        "Valor Total": `R$${valorTotal.toFixed(2).replace('.', ',')}`,
+        "Desconto Aplicado": `R$${descontoAplicado.toFixed(2).replace('.', ',')}`,
+        "Valor Final": `R$${valorFinal.toFixed(2).replace('.', ',')}`,
+        "Data de Compra": dataDaCompra.toLocaleDateString('pt-BR', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        })
+    }
+
+    if(quantidadeDeProdutos > 10 || valorFinal > 800.0) {
+        return console.table({
+            ...notaFiscal,
+            "Bônus": 'Você ganhou um cupom de R$50,00 para a sua prõxima compra'
+        })
+    } else {
+        return console.table(notaFiscal)
+    }
+}
+
+realizarCompra(clienteLilit)
