aafe3fe9f0995785c983c9a6df00b2f64b84759e
elizapimentel <elizapimentel@hotmail.com>
2022-05-02 16:51:35 -0300

    correções do desafio

--- desafio.js
+++ desafio.js
@@ -23,33 +23,40 @@ let clienteYolanda = [
     {produto: 'Short verde canelado', valor: 80.0}, 
     {produto: 'Salto agulha 39', valor: 250.0}, 
     {produto: 'Tênis casual preto', valor: 120.0}, 
-    {produto: 'meia calça transparente', valor: 30.0}]    
-
+    {produto: 'meia calça transparente', valor: 30.0}]   
+    
+    
+function calcularTotal(cliente){
+    let valorTotal = 0
+    for(i=0; i < cliente.length; i++) {
+        valorTotal += cliente[i].valor++
+    }
+    return valorTotal 
+    /*  valorTotal = cliente.reduce((acesso, produto) => acesso + produto.valor, 0)  
+    console.log(valorTotal) */ 
+}    
 
 function calcularCompras(cliente) {
-    let valorTotal = 0
     let valorDesconto = 0
     let valorFinal = 0
-    let quantidadeProdutos = cliente.length
-    
-    for(let item of cliente) {
-        valorTotal = cliente.reduce((acesso, produto) => acesso + produto.valor, 0)    
-        if(item.valor >= 200) {
-            valorDesconto += (item.valor*0.5) 
-            valorFinal += (item.valor - valorDesconto)
-        }else if(item.valor >= 100) {
-            valorDesconto += (item.valor*0.2)
-            valorFinal += (item.valor - valorDesconto)
-        }else if(item.valor >=80) {
-            valorDesconto += (item.valor*0.1)
-            valorFinal += (item.valor - valorDesconto)
-        }else if(item.valor >= 50) {
-            valorDesconto += (item.valor*0.05)
-            valorFinal += (item.valor - valorDesconto)
-        }else {
-            valorFinal += item.valor
+    let quantidadeProdutos = cliente.length   
+
+for(let item of cliente) {
+   if(item.valor >= 200.0) {
+       valorDesconto+=(item.valor*0.5)
+       } else if(item.valor >= 100.0) {
+           valorDesconto+=(item.valor*0.2)
+           } else if(item.valor >=80.0) {
+               valorDesconto+=(item.valor*0.1)
+               }else if(item.valor >= 50.0) {
+                   valorDesconto+=(item.valor*0.05)
         }
-    }
+        
+    } 
+
+let valorTotal = calcularTotal(cliente)    
+
+valorFinal = valorTotal-valorDesconto
 
 let dataCompra = new Date()
 
@@ -74,6 +81,7 @@ let notaFiscal = {
     } else{
         return console.table(notaFiscal)
     }
+
 }
 console.log("Compras cliente Lilit")
 calcularCompras(clienteLilit)
